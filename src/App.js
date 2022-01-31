9acfa6920f31d498344089a310f3edbaa279f862
worldOfRavi <98685697+worldOfRavi@users.noreply.github.com>
2022-01-31 17:56:13 +0545

    app.js page updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React from 'react';
 import "./App.css"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, Navigate} from 'react-router-dom'
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -32,7 +32,7 @@ function App() {
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/service' element={<Service/>} />
-        <Route path='*' element={<Error/>} />
+        <Route path='*' element={<Navigate to="/" />} />
       </Routes>
       <Footer />
      </>
